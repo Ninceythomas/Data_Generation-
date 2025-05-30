d7d656e7a8375c4c4f233728559b541d6d49b123
Ninceythomas <164032979+Ninceythomas@users.noreply.github.com>
2025-05-30 13:04:41 +0530

    Update expose.py

--- ECOM/expose.py
+++ ECOM/expose.py
@@ -3,6 +3,7 @@ from main.all import Ecom
 if __name__ == "__main__":
     ecommerce = Ecom()
     ecommerce.setup_data()
+    ecommerce.generate_orders(num_orders = 100000)
     print("Inserted 100 shops, 10,000 customers, and 10,000 stock items.")
-    ecommerce.evaluate_averages()
+
     
